17461e2e8ad69b80f25df9109572d6fc9dce4d50
MOHD NEHAL KHAN <nehalk482@gmail.com>
2025-07-26 17:14:05 +0530

    Build the routes logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
-import Login from './pages/auth/Login'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './App.css';
+import { Provider } from 'react-redux';
+import store from './store/store.js';
+import { RouterProvider, createBrowserRouter } from 'react-router';
 
-function App() {
-  return (
-    <Login />
-  )
-}
+import BuyerRoutes from './pages/buyer/Index.jsx';   
+import SellerRoutes from './pages/seller/Index.jsx'; 
+import StartPage from './pages/home/StartPage.jsx';
+import Login from './pages/auth/Login';
+import Signup from './pages/auth/Signup';
+import AuthLayout from './layout/AuthLayout.jsx';
 
-export default App
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <StartPage />
+      },
+      {
+        path: '/login',
+        element: (
+          <AuthLayout authentication={false}>
+            <Login />
+          </AuthLayout>
+        )
+      },
+      {
+        path: '/signup',
+        element: (
+          <AuthLayout authentication={false}>
+            <Signup />
+          </AuthLayout>
+        )
+      },
+      {
+        path: '/buyer/*',
+        element: <BuyerRoutes />
+      },
+      {
+        path: '/seller/*',
+        element: <SellerRoutes />
+      }
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
